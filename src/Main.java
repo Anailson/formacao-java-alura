5391a2eaaad6b808bcf9496316f25e62cfbf4ff7
Anailson Ribeiro dos Santos <anailson.santos@corregedoria.df>
2023-04-05 13:46:43 -0300

    refactor: alura

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluindoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """ 
+                    Filme Top Gun
+                    Filme de aventura 
+                    Muito bom!!!
+                    Ano de lançamento
+                """ + anoDeLancamento; /*Recurso apartir do Java 15*/
+        System.out.println(sinopse);
+
     }
 }
